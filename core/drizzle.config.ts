2e6413ba35659ada1c67e5037fa6a351b4b2691f
jonz94 <jody16888@gmail.com>
2024-08-31 01:20:07 +0800

    fix: correct typo

--- core/drizzle.config.ts
+++ core/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 import { env } from './env'
 
 export default defineConfig({
-  schema: './db/schema.ts',
+  schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'sqlite',
   driver: 'turso',
