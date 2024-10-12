ed065e6c25d71ccff00617ac631604cace36f51b
jonz94 <jody16888@gmail.com>
2024-10-13 07:41:32 +0800

    feat: upgrade `drizzle` packages to latest version

--- core/drizzle.config.ts
+++ core/drizzle.config.ts
@@ -3,9 +3,9 @@ import { env } from './env'
 
 export default defineConfig({
   schema: './src/db/schema.ts',
+  casing: 'snake_case',
   out: './drizzle',
-  dialect: 'sqlite',
-  driver: 'turso',
+  dialect: 'turso',
   dbCredentials: {
     url: env.DATABASE_URL,
     authToken: env.DATABASE_AUTH_TOKEN,
