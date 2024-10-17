44b538ab260c2fe379b4529556f012e04a06eaf7
jonz94 <jody16888@gmail.com>
2024-10-17 21:19:33 +0800

    feat: upgrade `drizzle` packages to latest version

--- core/src/db/db.ts
+++ core/src/db/db.ts
@@ -1,8 +1,12 @@
-import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 import { env } from '~/env'
 import * as schema from './schema'
 
-const client = createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN })
-
-export const db = drizzle(client, { schema })
+export const db = drizzle({
+  connection: {
+    url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
+  },
+  schema,
+  casing: 'snake_case',
+})
